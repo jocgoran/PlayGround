6a9f19313af8147bc236318b9c483fa1b254359b
Goran Joc <jocgrn@hotmail.com>
2021-01-13 00:35:40 +0100

    Update MultiThreading.cs
    add cross variables

--- QuickTests/ViewModel/CrossThreadingVarAccess/MultiThreading.cs
+++ QuickTests/ViewModel/CrossThreadingVarAccess/MultiThreading.cs
@@ -9,9 +9,14 @@ namespace QuickTests.ViewModel.CrossThreadingVarAccess
 {
     class MultiThreading
     {
+        public string InstanceVarOfMainThread = "Initialized in MainThread";
 
         public MultiThreading()
         {
+            string LocalMainThread = "Local variable, set in function of main thread";
+
+            App.mw.CrossThreadingVarAccess.ConsoleInnerText3 += $"In Thread {Thread.CurrentThread.Name}, BEFORE Thread.Start(): InstanceVarOfMainThread={InstanceVarOfMainThread}\n";
+
             Thread t1 = new Thread(Method1); 
             Thread t2 = new Thread(Method2);
             t1.Name = "Thread Nro 1";
@@ -19,8 +24,12 @@ namespace QuickTests.ViewModel.CrossThreadingVarAccess
             t1.Start();
             t2.Start();
 
-            t1.Join();
+            App.mw.CrossThreadingVarAccess.ConsoleInnerText3 += $"In Thread {Thread.CurrentThread.Name}, AFTER Thread.Start(): InstanceVarOfMainThread={InstanceVarOfMainThread}\n";
+
+            //t1.Join();
             t2.Join();
+
+            App.mw.CrossThreadingVarAccess.ConsoleInnerText3 += $"In Thread {Thread.CurrentThread.Name}, AFTER Thread.Join(): InstanceVarOfMainThread={InstanceVarOfMainThread}\n";
         }
 
         private void Method1()
@@ -31,6 +40,9 @@ namespace QuickTests.ViewModel.CrossThreadingVarAccess
                 App.mw.CrossThreadingVarAccess.ConsoleInnerText1 += $"Thread {Thread.CurrentThread.Name} interaction Nro {i}\n";
                 App.mw.CrossThreadingVarAccess.ConsoleInnerText3 += $"Thread {Thread.CurrentThread.Name} interaction Nro {i}\n";
             }
+
+            InstanceVarOfMainThread = $"Modified by {Thread.CurrentThread.Name}";
+            App.mw.CrossThreadingVarAccess.ConsoleInnerText3 += $"In Thread {Thread.CurrentThread.Name}, TERMINATED ELABORATION: InstanceVarOfMainThread={InstanceVarOfMainThread}\n";
         }
 
         private void Method2()
@@ -41,6 +53,9 @@ namespace QuickTests.ViewModel.CrossThreadingVarAccess
                 App.mw.CrossThreadingVarAccess.ConsoleInnerText2 += $"Thread {Thread.CurrentThread.Name} interaction Nro {i}\n";
                 App.mw.CrossThreadingVarAccess.ConsoleInnerText3 += $"Thread {Thread.CurrentThread.Name} interaction Nro {i}\n";
             }
+
+            InstanceVarOfMainThread = $"Modified by {Thread.CurrentThread.Name}";
+            App.mw.CrossThreadingVarAccess.ConsoleInnerText3 += $"In Thread {Thread.CurrentThread.Name}, TERMINATED ELABORATION: InstanceVarOfMainThread={InstanceVarOfMainThread}\n";
         }
     }
 }
